a727f10e25ea3324211d42d39c90679f80e9aa92
asethi0 <ashish.sethi101@gmail.com>
2017-11-06 10:24:19 -0800

    added function to get permission

--- tar_funcs.c
+++ tar_funcs.c
@@ -78,6 +78,18 @@ void write_dir(char* dirName, char* writeFile)
 }		
 
 					
+int get_permissions(char* path)
+{
+	 struct stat ret;
+    if (stat(path, &ret) == -1) {
+        printf("unable to access file\n");
+			exit(1);
+    }
+
+ return (ret.st_mode & S_IRUSR)|(ret.st_mode & S_IWUSR)|(ret.st_mode & S_IXUSR)|/*owner*/
+        (ret.st_mode & S_IRGRP)|(ret.st_mode & S_IWGRP)|(ret.st_mode & S_IXGRP)|/*group*/
+        (ret.st_mode & S_IROTH)|(ret.st_mode & S_IWOTH)|(ret.st_mode & S_IXOTH);/*other*/
+}
 
 	
 		
